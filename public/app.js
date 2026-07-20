ef68e2b564c1f6812f2b6a855af3923d76aa0b1b
MrChris-2025 <7rings@protonmail.com>
2026-07-20 09:30:28 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,6 @@
-q
+// Change this line inside app.js
+await fetch('/.netlify/functions/save-subscription', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(subscription)
+});
