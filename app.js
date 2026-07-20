093d1b08bad9fabf645ff8b3f4b97fa20c44a6b3
MrChris-2025 <7rings@protonmail.com>
2026-07-20 09:31:06 -0400

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// Change this line inside app.js
-await fetch('/.netlify/functions/save-subscription', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify(subscription)
-});
